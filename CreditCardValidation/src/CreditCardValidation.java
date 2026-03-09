bf990de6a5871eeaaa2cec90c84851f30396af9e
julia.dwyer <julia.dwyer@MU-C1-12.mullen.pvt.k12.co.us>
2026-03-09 14:31:38 -0600

    imported scanner and set up while loop

--- CreditCardValidation/src/CreditCardValidation.java
+++ CreditCardValidation/src/CreditCardValidation.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 
 public class CreditCardValidation
 	{
-
+		
+	
+		public static void readCards() throws IOException{
+			Scanner cardNums = new Scanner(new File("cardNumbers.txt"));
+			
+			while(cardNums.hasNext()) {
+				
+			}
+		}
 	}
