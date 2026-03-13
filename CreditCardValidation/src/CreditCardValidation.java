340d7fa3acfe107cb5e102e35c0760ca5786f4df
julia <julia@MillerDell>
2026-03-13 09:27:42 -0600

    layout of code

--- CreditCardValidation/src/CreditCardValidation.java
+++ CreditCardValidation/src/CreditCardValidation.java
@@ -2,16 +2,55 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
+public class CreditCardValidation {
 
-public class CreditCardValidation
-	{
-		
-	
-		public static void readCards() throws IOException{
-			Scanner cardNums = new Scanner(new File("cardNumbers.txt"));
-			
-			while(cardNums.hasNext()) {
-				
+	boolean valid = false;
+
+	System.out.println("What would you like to do?");System.out.println("1. Enter a credit card number");System.out.println("2. See list of credit card numbers");
+
+	Scanner choose = new Scanner(System.in);
+	int choice = choose.nextInt();
+
+	if(choice==1){
+		System.out.println("Please enter a 16 digit card number");
+	Scanner enterNum = new Scanner(System.in);
+		long enteredNum = enterNum.nextLong();
+	}
+		else if(choice== 2) {
+
+	public static void readCards() throws IOException {
+		Scanner cardNums = new Scanner(new File("cardNumbers.txt"));
+
+		while (cardNums.hasNext()) {
+
+			long number = cardNums.nextLong();
+			long[] cardNum = new long[16];
+
+			for (long i = 0; i < 16; i++) {
+				//digit stripping to put it into array?
+				cardNum[i] = number;
 			}
+
+			System.out.println(cardNum);
+
 		}
 	}
+
+	}
+
+	public static void testCards() {
+		for (long i = 0; i < 16; i++) {
+			if (i % 2 == 0) {
+				cardNums[i] = cardNums[i] + cardNums[i];
+				// idk how to do the if there are more then one digit
+			}
+		}
+		long sum = 0;
+		for (long i = 0; i < 16; i++) {
+			sum = sum + cardNums[i];
+		}
+		if (sum % 10 == 0) {
+			valid = true;
+		}
+	}
+}
